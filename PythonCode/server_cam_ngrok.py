ab0112740c949b0dae1922bd6362fea9d8662e21
Ramon27802092 <30840592+Ramon27802092@users.noreply.github.com>
2023-01-07 21:36:09 -0600

    Update server_cam_ngrok.py

--- PythonCode/server_cam_ngrok.py
+++ PythonCode/server_cam_ngrok.py
@@ -3,6 +3,13 @@ from flask import render_template
 from flask import Response
 import cv2
 from pyngrok import ngrok
+import firebase_admin
+import json
+import time
+from firebase_admin import credentials
+from firebase_admin import db
+import pyrebase 
+
 
 """
 Tunel con ngrok 
@@ -13,10 +20,43 @@ http_tunnel = ngrok.connect(5000,"http")
 tunnels = ngrok.get_tunnels() # Obtenemos los tuneles abiertos
 vpn = tunnels[1].__str__()    # Guardamos en formato string el valor 1 de la lista 
 VpnNgrok=vpn[vpn.find('http'):vpn.find('ngrok.io')+8]  # Otenemos solo la url
-#print(VpnNgrok)
+print(VpnNgrok)
+
 """
 Conexion con base de datos para enviar 
 """
+# Estructura de configuracion base de datos para almacenar imagenes
+config = {
+  "apiKey": "b6fdc2f949668840e5c5700dc4fd8a3a2dee3186",
+  "authDomain": "ventanaiot-56921.appspot.com",
+  "databaseURL": "https://accounts.google.com/o/oauth2/auth",
+  "storageBucket": "ventanaiot-56921.appspot.com"
+}
+
+# Configuracion de base de datos general  
+cred = credentials.Certificate('firebase-sdk.json')
+firebase_admin.initialize_app(cred,{
+    'databaseURL' : 'https://ventanaiot-56921-default-rtdb.firebaseio.com/'
+})
+
+# Config base de datos de archivos (imagenes)
+firebase = pyrebase.initialize_app(config)
+
+storage = firebase.storage()
+
+db=firebase.database()
+
+"""
+with open('Credenciales.json') as f: 
+        data = json.load(f)
+
+user =  data['usuario']  # Se obtiene valor de usuario 
+
+db=firebase.database()
+
+usuario = 'usuario'+user
+"""
+db.child("LoginSignup").child("Ramon1644").child("urls").child("ipglobal").update(VpnNgrok)
 
 app = Flask(__name__)
 cap = cv2.VideoCapture(1)
@@ -24,10 +64,13 @@ cap = cv2.VideoCapture(1)
 def generate():
      while True:
           success, frame = cap.read()
-          tunnels = ngrok.get_tunnels() # Obtenemos los tuneles abiertos
-          vpn = tunnels[1].__str__()    # Guardamos en formato string el valor 1 de la lista 
-          VpnNgrok=vpn[vpn.find('http'):vpn.find('ngrok.io')+8]  # Otenemos solo la url
-          print(VpnNgrok)
+          """
+          Parece ser no de utilidad aquí 
+          #tunnels = ngrok.get_tunnels() # Obtenemos los tuneles abiertos
+          #vpn = tunnels[1].__str__()    # Guardamos en formato string el valor 1 de la lista 
+          #VpnNgrok=vpn[vpn.find('http'):vpn.find('ngrok.io')+8]  # Otenemos solo la url
+          #print(VpnNgrok)
+          """
           if not success:
                break
           else:
